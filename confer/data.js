ce07197c74d2c3068e33a9d117c463b16e341ddc
zhangxs <972678896@qq.com>
2020-07-17 08:59:53 +0800

    html

---
+++ confer/data.js
@@ -0,0 +1,62 @@
+window.data = {
+  events: [
+    {
+      id: 1,
+      name: "会议001",
+      organizer: "组织者001",
+      time: "2020-01-01",
+    },
+    {
+      id: 2,
+      name: "会议002",
+      organizer: "组织者002",
+      time: "2020-01-05",
+    },
+  ],
+  tableData: [
+    {
+      id: 1,
+      name: "会议1",
+      room: "Hall 1",
+      time9: {
+        name: "Diffasdlpo pllsda901",
+        isTalk: false,
+        isWorkshop: true,
+      },
+      time11: "Diffasdlpo pllsd",
+      time13: "Diffasdlpo pllsd",
+      time15: "Diffasdlpo pllsd",
+    },
+    {
+      id: 2,
+      name: "会议2",
+      room: "Hall 1",
+      time9: "Diffasdlpo pllsd",
+      time11: "",
+      time13: "Diffasdlpo pllsd",
+      time15: "Diffasdlpo pllsd",
+    },
+    {
+      id: 3,
+      name: "会议4",
+      room: "Hall 1",
+      time9: "Diffasdlpo pllsd",
+      time11: "Diffasdlpo pllsd",
+      time13: {
+        name: "Diffasdlpo pllsda901",
+        isTalk: false,
+        isWorkshop: true,
+      },
+      time15: "Diffasdlpo pllsd",
+    },
+    {
+      id: 4,
+      name: "会议3",
+      room: "Hall 1",
+      time9: "Diffasdlpo pllsd",
+      time11: "Diffasdlpo pllsd",
+      time13: "Diffasdlpo pllsd",
+      time15: "Diffasdlpo pllsd",
+    },
+  ],
+};
